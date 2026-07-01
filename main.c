01af341f3a406e08b7a1f960ef916547fd82c900
AlexisDabon <xela619375@gmail.com>
2026-07-01 05:57:18 +0200

    feat: while loops

--- main.c
+++ main.c
@@ -11,4 +11,20 @@ int main () {
 	int code_secret = 1234;
 	int echecs = 0;
 	int saisis_utilisateur = 0;
+	
+	while (echecs < 3) {
+		printf("taper le code d'accès \n");
+		scanf("%d", &saisis_utilisateur);
+
+	if (saisis_utilisateur == code_secret) {
+		printf("code correct \n");
+		break;
+	}
+	else { 
+		echecs++;
+		printf("erreur \n");
+	} 
+}
+
+	
 

695ad55ed5093dc7f8d6d10e54177f5af2eb6802
AlexisDabon <xela619375@gmail.com>
2026-07-01 16:25:51 +0200

    feat: menu

--- main.c
+++ main.c
@@ -8,12 +8,13 @@ struct Balise {
 int main () {
 	struct Balise ma_balise;
 		ma_balise.frequence = 150.0;
-		ma_balise.statut_signal = 1;
+ 		ma_balise.statut_signal = 1;
 	
 	int code_secret = 1234;
 	int echecs = 0;
 	int saisis_utilisateur = 0;
-	
+	int choix = 0;
+
 	while (echecs < 3) {
 		printf("taper le code d'accès \n");
 		scanf("%d", &saisis_utilisateur);
@@ -33,6 +34,15 @@ int main () {
 	}
 
 	printf("Access \n");
+	
+	printf("frequence : %.2f \n", ma_balise.frequence);
+	printf("statut_signal : %d \n", ma_balise.statut_signal);
+
+	printf("1.modifier frequence : \n");
+	printf("2.Deconnexion \n");
+	printf("choix : \n");
+	scanf("%d", &choix);
+
 
 return 0;
 
